ff666ed4182f5c705247d543153366159014d13c
Keraiink <Monica_cool@live.dk>
2020-12-09 18:56:10 +0100

    Ændring
    Ændre på animation

--- Hjemmeside/javascript.js
+++ Hjemmeside/javascript.js
@@ -9,11 +9,8 @@ const navSlide = () => {
 
     //Animste links
       navlinks.forEach((link, index) => {
-        if(link.style.animation){
-          link.style.animation = '';
-        } else {
-          link.style.animation = `navlinkfade 10s ease forwards ${index/5+0.5}s`;
-        }
+        link.style.animation = `navLinkFade 0.5s ease forward ${index/7}s`;
+        console.log{index/7}
     });
     //Burger animation
     burger.classList.toggle('toggle');
