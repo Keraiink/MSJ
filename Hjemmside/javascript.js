0d4edef04bbdbc65b092e4cb9c064167327c78bf
Keraiink <Monica_cool@live.dk>
2020-12-09 16:52:06 +0100

    Sat Javascript
    Sætter Javascript ind

---
+++ Hjemmside/javascript.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navlinks = document.querySelectorAll('.nav-links li');
+
+  //toggle item
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    //Animste links
+      navlinks.forEach((link, index) => {
+        if(link.style.animation){
+          link.style.animation = '';
+        } else {
+          link.style.animation = `navlinkfade 10s ease forwards ${index/5+0.5}s`;
+        }
+    });
+    //Burger animation
+    burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
